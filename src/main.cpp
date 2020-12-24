989fc5ffa81a0d8abe280cba23cdc2d405ee3138
bilalguvenc <bilalguvenc98@gmail.com>
2020-12-24 19:23:30 +0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <Servo.h>
-#include "devices.h"
-#include "network.h"
-#include "mytime.h"
-
+// #include "devices.h"
+// #include "network.h"
+// #include "mytime.h"
+//#include "packet.h"
 
 String ssid = "TacticForce";
 String pass = "Hes20181920";
@@ -13,22 +13,33 @@ void setup()
   Serial.begin(115200);
   Serial.println("");
 
-  MyDevices::init();
-  MyNetwork::init(ssid, pass);
+  // MyDevices::init();
+  // MyNetwork::init(ssid, pass);
+  //String pack = "asd";
+  //int in = 1;
+  //bool res = MyPacket::OnUpdateIoTState(pack, in);
 }
 
 void loop() 
 {
-  if (MyTime::isOneSecondPassed()) 
-  {
-    // float temp = MyDevices::GetTemperature();
-    // Create Packet
-    // MyNetwork::SendToAll(packet);
-    // Serial.print(" Sicaklik: ");
-    // Serial.println(temp);
-  }
+  // if (MyTime::isOneSecondPassed()) 
+  // {
+  //   // float temp = MyDevices::GetTemperature();
+  //   // Create Packet
+  //   // MyNetwork::SendToAll(packet);
+  //   // Serial.print(" Sicaklik: ");
+  //   // Serial.println(temp);
 
-  MyNetwork::Handle();
+  //   delay(10);
+
+    
+  //   // uint8_t a = 16;
+  //   // Serial.println(a);
+  // }
+  Serial.println("asd");
+  delay(100);
+  //MyNetwork::Handle();
+  //MyPacket::OnUpdateIoTState();
   
   // digitalWrite(device::O_Lamba, HIGH);
   // delay(1000);
